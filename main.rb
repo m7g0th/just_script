4e16bddc257410ec210580eae2f05d89d5d75c67
threepwoodg <ttnonick@gmail.com>
2018-05-18 21:22:42 +0300

    0.0.9

--- main.rb
+++ main.rb
@@ -4,24 +4,38 @@ require 'csv'
 require 'ruby-progressbar'
 require 'spreadsheet'
 
+system('cls') # windows
+system('reset') # linux
+
 #---
-# Parse by number v0.0.8
+# Parse by number v0.0.9
 #
 # parse content from service http://aclist.ru
+# parse content from service https://cheinomer.ru/
 #
 # threepwoodg
+# m7g
 #---
 
 def request(number)
-  Nokogiri::HTML(open("http://aclist.ru/site/check?phone=#{number.last}")).css('.content__title').each do |content|
-    @sheet1[@i,0] = number.last.to_i
-    @sheet1[@i,1] = content.content
-  end
+  aclist_parse = Nokogiri::HTML(open("http://aclist.ru/site/check?phone=#{number.last}"))
+  cheinomer_parse = Nokogiri::HTML(open("https://cheinomer.ru/telefona/?n=#{number.last}"))
+  number_content = aclist_parse.css('.content__title')
+  @sheet1[@i,0] = number.last.to_i
+  @sheet1[@i,1] = number_content[0].content
   begin
-    company = Nokogiri::HTML(open("https://cheinomer.ru/telefona/?n=#{number.last}")).css('.alert-info strong')
-    @sheet1[@i,2] = company[2].content.chop
-    Nokogiri::HTML(open("https://cheinomer.ru/telefona/?n=#{number.last}")).css('.tvebuttoncolor').each do |name|
-      @sheet1[@i,3] = name.content if /\+/=~name.content
+    company = cheinomer_parse.css('.alert-info strong')
+    @sheet1[@i,2] = company[2].content
+    extra = []
+    cheinomer_parse.css('.tvebuttoncolor').each do |dop|
+      extra << dop.content if /\+/=~dop.content
+    end
+    @sheet1[@i,3] = extra.last
+    region = cheinomer_parse.css('.entry-content h2')
+    if /Регион номера+/=~region[0].content
+      @sheet1[@i,4] = region[0].content
+    elsif /Регион номера+/=~region[1].content
+      @sheet1[@i,4] = region[1].content
     end
   rescue
     @sheet1[@i,2] = "Номер не найден в базе"
@@ -30,7 +44,7 @@ def request(number)
 end
 
 def read_file(path)
-  progressbar = ProgressBar.create(:format=> "%a %b\u{15E7}%i %p%% %t", :progress_mark  => ' ', :remainder_mark => "\u{FF65}", :starting_at => 0)
+  progressbar = ProgressBar.create(:format=> "%a %b\u{003E}%i %p%% %t", :progress_mark  => "\u{0023}", :remainder_mark => " ", :starting_at => 0)
   progressbar.total = open(path).read.count("\n")
   book = Spreadsheet::Workbook.new
   @sheet1 = book.create_worksheet
@@ -39,13 +53,14 @@ def read_file(path)
   @sheet1[0,1] = 'Результат с сайта aclist.ru'
   @sheet1[0,2] = 'Результат с сайта cheinomer.ru'
   @sheet1[0,3] = 'Дополнителная сведенья с сайта cheinomer.ru (если имеются)'
+  @sheet1[0,4] = 'Регион'
   @i = 1
   CSV.foreach(path) do |row|
     request(row)
     progressbar.increment
   end
   time = Time.new()
-  book.write "lib/#{time.strftime("%H:%M-%d-%m-%Y")}.xls"
+  book.write "lib/#{time.strftime("%Y-%m-%d-%H-%M-%S")}.xls"
 end
 
 read_file('lib/input.txt')
